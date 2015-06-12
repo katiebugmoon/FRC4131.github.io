a6554c889a393e4914238c9da5161538c6983783
endreman0 <endreman@squidgey.com>
2015-06-12 16:20:05 -0700

    Globalize slideshow parameters (2015 only, for testing)

--- res/media.js
+++ res/media.js
@@ -1,12 +1,12 @@
 var ALPHA_HIGH = 1.0, ALPHA_MEDIUM = 0.7, ALPHA_LOW = 0.4;
-var NUM_IMAGES = srcIcon.length;
+var NUM_IMAGES = window.slideshow_srcIcon.length;
 $(document).ready(function(){
 	var table = $("#link-table");
 	for(var i=0;i<NUM_IMAGES;){
 		var start=i;
 		var row = $("<tr></tr>");
 		for(;(i-start)<8 && i<NUM_IMAGES;i++){
-			var img = $("<td data-index='" + i + "'><img src='" + srcIcon[i] + "'/></td>");
+			var img = $("<td data-index='" + i + "'><img src='" + window.slideshow_srcIcon[i] + "'/></td>");
 			img.click(function(){
 				fadeOut($(".selected"));
 				fadeIn($(this));
@@ -32,7 +32,7 @@ function fadeIn(img){
 	img.fadeTo("slow", ALPHA_HIGH);
 	$("#main-wrapper").fadeTo("fast", 0, function(){
 		var ind = parseInt(img.attr("data-index"));
-		var src = srcMain[ind];
+		var src = window.slideshow_srcMain[ind];
 		if($("#main").tagName() == "img"){
 			console.log("img");
 			if(startsWith(src, "http")){
@@ -47,8 +47,8 @@ function fadeIn(img){
 			}
 		}
 		$("#main").attr("src", src);
-		$("#main-link").attr("href", href[ind]);
-		$("#caption").html(captions[ind]);
+		$("#main-link").attr("href", window.slideshow_href[ind]);
+		$("#caption").html(window.slideshow_captions[ind]);
 		$("#main-wrapper").fadeTo("fast", 1);
 	});
 }
