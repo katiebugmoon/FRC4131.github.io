9c059a35c37b9044686c4e5c4778c067e1c2988d
endreman0 <endreman@squidgey.com>
2015-06-11 19:46:53 -0700

    Derpfix: wrong selector

--- res/pics2015.js
+++ res/pics2015.js
@@ -1,8 +1,6 @@
 var NUM_IMAGES = 7;
-console.log("2015 Pictures JS loaded");
 $(document).ready(function(){
-  console.log("2015 Pictures JS running");
-  var table = $("link-table");
+  var table = $("#link-table");
   for(var i=0;i<NUM_IMAGES;){
     var start=i;
     var row = $("<tr></tr>");
