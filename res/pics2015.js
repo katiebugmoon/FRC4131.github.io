25bd175896e4dc389c2ecf574e6ba4812e8349d0
endreman0 <endreman@squidgey.com>
2015-06-11 19:44:10 -0700

    Some basic jQuery to stock the table

---
+++ res/pics2015.js
@@ -0,0 +1,12 @@
+var NUM_IMAGES = 7;
+$(document).ready(function(){
+  var table = $("link-table");
+  for(var i=0;i<NUM_IMAGES;){
+    var start=i;
+    var row = $("<tr></tr>");
+    for(;(i-start)<4;i++){
+      row.append($("<td><img src='img" + i + ".jpg'/></td>"));
+    }
+    table.append(row);
+  }
+});
