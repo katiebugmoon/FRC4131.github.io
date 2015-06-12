565fd5606d99b1268bbca72b4c87356732c6f1b0
endreman0 <endreman@squidgey.com>
2015-06-12 15:54:41 -0700

    Make all other media pages into slideshows
    Vimeo icon and embed functions, hopefully

--- res/media.js
+++ res/media.js
@@ -62,3 +62,11 @@ function fadeOut(img){
 }
 function ytIcon(id){return "https://img.youtube.com/vi/" + id + "/0.jpg";}
 function ytEmbed(id){return "https://www.youtube.com/embed/" + id + "?autoplay=0";}
+function vimeoIcon(id){
+	var url = null;
+	$.getJSON("https://vimeo.com/api/v2/video/" + id + ".json?callback=?", function(data){
+		url = data[0].thumbnail_large;
+	});
+	return url;
+}
+function vimeoEmbed(id){return "https://player.vimeo.com/video/" + id;}
