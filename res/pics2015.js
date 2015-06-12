bc9c2ae7173d5cf1551f77091e7daea0556bba49
endreman0 <endreman@squidgey.com>
2015-06-11 19:45:56 -0700

    Hmm...

--- res/pics2015.js
+++ res/pics2015.js
@@ -1,10 +1,12 @@
 var NUM_IMAGES = 7;
+console.log("2015 Pictures JS loaded");
 $(document).ready(function(){
+  console.log("2015 Pictures JS running");
   var table = $("link-table");
   for(var i=0;i<NUM_IMAGES;){
     var start=i;
     var row = $("<tr></tr>");
-    for(;(i-start)<4;i++){
+    for(;(i-start)<4 && i<NUM_IMAGES;i++){
       row.append($("<td><img src='img" + i + ".jpg'/></td>"));
     }
     table.append(row);
