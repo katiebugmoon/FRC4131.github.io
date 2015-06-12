aa341b138f3618042f7d8b39ebfc5d5867ea82ef
endreman0 <endreman@squidgey.com>
2015-06-12 11:58:39 -0700

    Switch from Atom to Sublime, and I now see that my files were 50% spaces and 50% tabs

--- res/pics2015.js
+++ res/pics2015.js
@@ -4,47 +4,47 @@ var captions = ["Caught Alex by surprise!", "Sydney was heads-down with business
 var src = ["img0.jpg", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg"];
 var href = ["img0.jpg", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "../quotes"];
 $(document).ready(function(){
-  var table = $("#link-table");
-  for(var i=0;i<NUM_IMAGES;){
-    var start=i;
-    var row = $("<tr></tr>");
-    for(;(i-start)<10 && i<NUM_IMAGES;i++){
-      var img = $("<img src='" + src[i] + "' data-index='" + i + "'/>");
-      img.click(function(){
-        fadeOut($(".selected"));
-        fadeIn($(this));
-      });
-      img.mouseenter(function(){
-        if(!$(this).hasClass("selected"))
-          $(this).fadeTo("slow", ALPHA_MEDIUM);
-      });
-      img.mouseleave(function(){
-        if($(this).hasClass("selected"))
-          $(this).fadeTo("slow", ALPHA_HIGH);
-        else
-        $(this).fadeTo("slow", ALPHA_LOW);
-      })
-      var cell = $("<td></td>");
-      cell.append(img);
-      row.append(cell);
-    }
-    table.append(row);
-  }
-  var imgs = $("#link-table img"), first = imgs.first();
-  fadeOut(imgs.not(first));
-  fadeIn(first);
+	var table = $("#link-table");
+	for(var i=0;i<NUM_IMAGES;){
+		var start=i;
+		var row = $("<tr></tr>");
+		for(;(i-start)<10 && i<NUM_IMAGES;i++){
+			var img = $("<img src='" + src[i] + "' data-index='" + i + "'/>");
+			img.click(function(){
+				fadeOut($(".selected"));
+				fadeIn($(this));
+			});
+			img.mouseenter(function(){
+				if(!$(this).hasClass("selected"))
+					$(this).fadeTo("slow", ALPHA_MEDIUM);
+			});
+			img.mouseleave(function(){
+				if($(this).hasClass("selected"))
+					$(this).fadeTo("slow", ALPHA_HIGH);
+				else
+					$(this).fadeTo("slow", ALPHA_LOW);
+			});
+			var cell = $("<td></td>");
+			cell.append(img);
+			row.append(cell);
+		}
+		table.append(row);
+	}
+	var imgs = $("#link-table img"), first = imgs.first();
+	fadeOut(imgs.not(first));
+	fadeIn(first);
 });
 function fadeIn(img){
-  img.toggleClass("selected", true);
-  img.fadeTo("slow", ALPHA_HIGH);
-  $("#main-wrapper").fadeTo("fast", 0, function(){
-	$("#main").attr("src", img.attr("src"));
-  $("#main-link").attr("href", href[parseInt($(".selected").attr("data-index"))]);
-	$("#caption").text(captions[parseInt($(".selected").attr("data-index"))]);
-	$("#main-wrapper").fadeTo("fast", 1);
-  });
+	img.toggleClass("selected", true);
+	img.fadeTo("slow", ALPHA_HIGH);
+	$("#main-wrapper").fadeTo("fast", 0, function(){
+		$("#main").attr("src", img.attr("src"));
+		$("#main-link").attr("href", href[parseInt($(".selected").attr("data-index"))]);
+		$("#caption").text(captions[parseInt($(".selected").attr("data-index"))]);
+		$("#main-wrapper").fadeTo("fast", 1);
+	});
 }
 function fadeOut(img){
-  img.toggleClass("selected", false);
-  img.fadeTo("slow", ALPHA_LOW);
+	img.toggleClass("selected", false);
+	img.fadeTo("slow", ALPHA_LOW);
 }
