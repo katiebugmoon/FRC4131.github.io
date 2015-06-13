ea629d05a33a26d7bd15e71f7074709fd98e1203
endreman0 <endreman@squidgey.com>
2015-06-12 18:24:01 -0700

    Revert "Futile attempt to load Vimeo icons before I rewrite it to use callbacks"
    This reverts commit 7938928459f0a0793a5ef47a3aea11a17cc7fde2.
    
    Conflicts:
    	res/media_util.js

--- res/media_util.js
+++ res/media_util.js
@@ -1,18 +1,3 @@
 function ytIcon(id){return "https://img.youtube.com/vi/" + id + "/0.jpg";}
 function ytEmbed(id){return "https://www.youtube.com/embed/" + id + "?autoplay=0";}
-function vimeoIcon(id){return "_VIMEO" + id;}//This will be hunted down later by getVimeoIcons().
 function vimeoEmbed(id){return "https://player.vimeo.com/video/" + id;}
-function getVimeoIcons(){
-	console.log("Finding Vimeo thumbnails");
-	$("[src|='_VIMEO']").each(function(ind, element){
-		console.log("Thumbnail found #" + ind);
-		var id = $(element).attr("id");
-		var vid = id.substring(6, id.length);
-		console.log("Finding Vimeo thumbnail " + vid);
-		$.getJSON('http://vimeo.com/api/v2/video/' + vid + '.json?callback=?', function(data){
-			var url = data[0].thumbnail_large;
-			console.log("URL=" + url);
-			$(element).attr("src", url);
-		});
-	});
-}
