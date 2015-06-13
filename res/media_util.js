2b9201bb6803159505e513cd9e3aaf69c4745846
endreman0 <endreman@squidgey.com>
2015-06-12 18:17:39 -0700

    Another log

--- res/media_util.js
+++ res/media_util.js
@@ -3,8 +3,8 @@ function ytEmbed(id){return "https://www.youtube.com/embed/" + id + "?autoplay=0
 function vimeoIcon(id){return "_VIMEO" + id;}
 function vimeoEmbed(id){return "https://player.vimeo.com/video/" + id;}
 function getVimeoIcons(){
-	var imgs = $("[src|=_VIMEO]");
-	imgs.each(function(ind, element){
+	console.log("Finding Vimeo thumbnails");
+	$("[src|=_VIMEO]").each(function(ind, element){
 		var id = $(element).attr("id");
 		var vid = id.substring(6, id.length);
 		console.log("Finding Vimeo thumbnail " + vid);
