7938928459f0a0793a5ef47a3aea11a17cc7fde2
endreman0 <endreman@squidgey.com>
2015-06-12 17:55:05 -0700

    Futile attempt to load Vimeo icons before I rewrite it to use callbacks

--- res/media_util.js
+++ res/media_util.js
@@ -1,13 +1,9 @@
 function ytIcon(id){return "https://img.youtube.com/vi/" + id + "/0.jpg";}
 function ytEmbed(id){return "https://www.youtube.com/embed/" + id + "?autoplay=0";}
 function vimeoIcon(id){
-	var url = null;
 	console.log("Getting thumbnail for Vimeo #" + id);
-	$.getJSON("https://vimeo.com/api/v2/video/" + id + ".json?callback=?", function(data){
-		url = data[0].thumbnail_large;
-		console.log("Vimeo URL (handler): " + url);
+	return $.getJSON("https://vimeo.com/api/v2/video/" + id + ".json?callback=?").then(function(data){
+		return data[0].thumbnail_large;
 	});
-	console.log("Vimeo URL (function): " + url);
-	return url;
 }
 function vimeoEmbed(id){return "https://player.vimeo.com/video/" + id;}
