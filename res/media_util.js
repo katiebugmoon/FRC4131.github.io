39cfb877236273a602374b37978b384af6dc8f45
endreman0 <endreman@squidgey.com>
2015-06-12 18:19:19 -0700

    Another log and potentially derpfix: quotes around value in jQuery selector

--- res/media_util.js
+++ res/media_util.js
@@ -1,10 +1,11 @@
 function ytIcon(id){return "https://img.youtube.com/vi/" + id + "/0.jpg";}
 function ytEmbed(id){return "https://www.youtube.com/embed/" + id + "?autoplay=0";}
-function vimeoIcon(id){return "_VIMEO" + id;}
+function vimeoIcon(id){return "_VIMEO" + id;}//This will be hunted down later by getVimeoIcons().
 function vimeoEmbed(id){return "https://player.vimeo.com/video/" + id;}
 function getVimeoIcons(){
 	console.log("Finding Vimeo thumbnails");
-	$("[src|=_VIMEO]").each(function(ind, element){
+	$("[src|='_VIMEO']").each(function(ind, element){
+		console.log("Thumbnail found #" + ind);
 		var id = $(element).attr("id");
 		var vid = id.substring(6, id.length);
 		console.log("Finding Vimeo thumbnail " + vid);
