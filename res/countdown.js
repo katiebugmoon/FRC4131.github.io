1544162ec445f7068cbab28342c718511a2d97ad
endreman0 <ian.uu12@gmail.com>
2016-10-29 15:46:43 -0700

    May have found the problem

--- res/countdown.js
+++ res/countdown.js
@@ -3,8 +3,8 @@ var countdownDate = new Date(2017, 0, 7);
 $(function(){
 	var delta = countdownDate.getTime() - Date.now();
 	var ms = delta;
-	var s = Math.floor(ms / 100);
-	ms %= 100;
+	var s = Math.floor(ms / 1000);
+	ms %= 1000;
 	var m = Math.floor(s / 60);
 	s %= 60;
 	var h = Math.floor(m / 60);
