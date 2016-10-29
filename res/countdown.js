8055ab3b26c7dbb167741b98a1c51859a9caf397
endreman0 <ian.uu12@gmail.com>
2016-10-29 15:28:30 -0700

    Flipped signs

--- res/countdown.js
+++ res/countdown.js
@@ -1,6 +1,14 @@
 var countdownTarget = 'Kickoff 2017';
 var countdownDate = new Date(2017, 0, 7);
 $(function(){
-	var delta = Date.now() - countdownDate.valueOf();
-	console.log(Date.now(), Date.now.valueOf(), countdownDate, countdownDate.valueOf(), delta);
+	var ms = countdownDate.valueOf() - Date.now();
+	var s = Math.floor(ms / 100);
+	ms %= 100;
+	var m = Math.floor(s / 60);
+	s %= 60;
+	var h = Math.floor(m / 60);
+	m %= 60;
+	var d = Math.floor(h / 24);
+	h %= 24;
+	console.log(d, h, m, s, ms);
 });
