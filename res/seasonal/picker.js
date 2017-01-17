ff52af3870726b389e9b3ea32a3ca080f6deb7d6
Ian W <ian.uu12@gmail.com>
2017-01-16 20:30:35 -0800

    Seasonal formatting picker

---
+++ res/seasonal/picker.js
@@ -0,0 +1,31 @@
+---
+---
+(function(holidays){
+	var urlPrefix = '{{ site.baseurl }}/res/seasonal/';
+	Date now = new Date();
+	var month = now.getMonth() + 1, day = now.getDate();
+	for(var i=holidays.length-1; i>=0; --i){
+		var holiday = holidays[i];
+		var afterStart = month >= holiday.start[0] && day >= holiday.start[1],
+			beforeEnd = month <= holiday.end[0] && day <= holiday.end[1];
+		
+		var inRange;
+		if(holiday.start[0] < holiday.end[0]) inRange = afterStart || beforeEnd;//Across years (e.g. December into January)
+		else inRange = afterStart && beforeEnd;//Within one year
+		
+		if(inRange){
+			var el = document.createElement('link');
+			el.setAttribute('rel', 'stylesheet');
+			el.setAttribute('type', 'text/css');
+			el.setAttribute('href', urlPrefix + holiday.file);
+		}
+	}
+})([{
+	'start': [10, 1],
+	'end': [10, 31],
+	'file': 'halloween.css'
+}, {
+	'start': [12, 1],
+	'end': [1, 7],
+	'file': 'firstmas.css'
+}]);
